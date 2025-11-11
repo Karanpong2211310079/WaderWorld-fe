ed4cc0b087034fb06abae4aa9280404fca1810d2
Karanpong2211310079 <tankangamer@gmail.com>
2025-11-11 20:36:40 +0700

    feat: Add group invite and people components with basic templates and tests; update group component layout

---
+++ src/app/shared/components/group-post-list/group-post-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-post-list',
+  imports: [],
+  templateUrl: './group-post-list.component.html',
+  styleUrl: './group-post-list.component.scss'
+})
+export class GroupPostListComponent {
+
+}
--- src/app/view/Home/pages/group/group.component.ts
+++ src/app/view/Home/pages/group/group.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
-import { CreatePostComponent } from "../../../../shared/components/create-post/create-post.component";
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { CreatePostComponent } from '../../../../shared/components/create-post/create-post.component';
 import { FollowBtnComponent } from '../../../../shared/components/follow-btn/follow-btn.component';
+import { Subject } from 'rxjs';
+import { AuthenticationServiceService } from '../../../../core/service/authentication-service/authentication-service.service';
+import { RestApiService } from '../../../../core/service/rest-api-service/rest-api.service';
 @Component({
   selector: 'app-group',
-  imports: [CreatePostComponent,FollowBtnComponent],
+  imports: [CreatePostComponent, FollowBtnComponent],
   templateUrl: './group.component.html',
-  styleUrl: './group.component.scss'
+  styleUrl: './group.component.scss',
 })
-export class GroupComponent {
-  condition = 1
+export class GroupComponent implements OnDestroy, OnInit {
+  private restapi = inject(RestApiService);
+  private authen = inject(AuthenticationServiceService);
+  private unsubscribe$ = new Subject<void>();
 
-  changeStateIndex(i:number){
-    this.condition = i
+  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  condition = 1;
+
+  changeStateIndex(i: number) {
+    this.condition = i;
   }
 }
