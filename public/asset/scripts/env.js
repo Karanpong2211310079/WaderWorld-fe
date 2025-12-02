5c34668df2b627c10072bc063f7a8c4366f5a8b1
Thachpol <thachpol2547@gmail.com>
2025-12-02 23:52:07 +0700

    book

--- public/asset/scripts/env.js
+++ public/asset/scripts/env.js
@@ -1,5 +1,5 @@
 (function (window) {
   window.__env = window.__env || {};
-  window.__env.apiUrl = "http://localhost:8000/api/";
+  window.__env.apiUrl = "https://warwold-be-01.onrender.com/api/";
   window.__env.production = false;
 })(this);
