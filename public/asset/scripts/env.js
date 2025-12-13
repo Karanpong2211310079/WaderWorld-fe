826c37e7f0c0fbe28e436c01120b92454288a16f
Karanpong2211310079 <tankangamer@gmail.com>
2025-12-13 17:53:58 +0700

    zsx

--- public/asset/scripts/env.js
+++ public/asset/scripts/env.js
@@ -1,5 +1,5 @@
 (function (window) {
   window.__env = window.__env || {};
-  window.__env.apiUrl = "http://localhost:8000/api/";
+  window.__env.apiUrl = "https://warwold-be-6.onrender.com";
   window.__env.production = false;
 })(this);
