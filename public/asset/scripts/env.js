2f8d45f2134ce1ee858dd2416d352f82a21cc969
Karanpong2211310079 <tankangamer@gmail.com>
2025-12-14 10:47:40 +0700

    Fix API URL references by adding trailing slashes; update cache and environment scripts

--- public/asset/scripts/env.js
+++ public/asset/scripts/env.js
@@ -1,5 +1,5 @@
 (function (window) {
   window.__env = window.__env || {};
-  window.__env.apiUrl = "https://warwold-be-6.onrender.com"; // ไม่ใช่ localhost
+  window.__env.apiUrl = "https://warwold-be-6.onrender.com/"; // ไม่ใช่ localhost
   window.__env.production = false;
 })(this);
